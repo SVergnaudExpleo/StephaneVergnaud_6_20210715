68083b408ab804c33ba3dee3d11d376ff4d8d333
SVergnaudExpleo <80250723+SVergnaudExpleo@users.noreply.github.com>
2021-08-25 15:52:23 +0200

    commit

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,3 +1,7 @@
+import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
+import {getByTestId} from '@testing-library/dom'
+
 import { screen } from "@testing-library/dom"
 import {default as BillsUI} from "../views/BillsUI.js"
 import {default as NewBillUI } from "../views/NewBillUI"
@@ -8,26 +12,45 @@ import {default as NewBillContainer} from "../containers/NewBill"
 import {default as LoadingPage} from "../views/LoadingPage"
 import {default as VerticalLayout} from "../views/VerticalLayout"
 import {ROUTES as ROUTES, ROUTES_PATH as ROUTES_PATH} from "../constants/routes.js"
+
+
 import {default as Router} from "../app/Router"
+
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
 
     // on test que l'icone est bien mis en valeur
-    test("Then bill icon in vertical layout should be highlighted", () => {
+    it("Then bill icon in vertical layout should be highlighted", async () => {
 
-      VerticalLayout(120)
-      const html = BillsUI({ data: [] })
-      document.body.innerHTML = html
+      document.body.innerHTML = BillsUI({ data: [] })
+      VerticalLayout()
+      document.location = '/'
+
+      const test = document
+      console.log("je suis un test = "+test)
+      
+  
       //to-do write expect expression
-      //expect(html).toContain("active-icon"); // id = "layout-icon1" test id = "icon-window" la className doit être "active-icon"
-      expect(1).toEqual(5)
-    })
+      //expect(document.querySelector('#layout-icon1').className).toContain("active-icon"); // id = "layout-icon1" test id = "icon-window" la className doit être "active-icon"
+      expect(document).toEqual(VerticalLayout(120))
+  })
+
+
+
+
+
+
+
+
+
+
+
 
     // on test que le tri decroissant est bien effectué
     // on compare les dates (normalement triées) avec les dates triées par le test
-    test("Then bills should be ordered from earliest to latest", () => {
+    it("Then bills should be ordered from earliest to latest", () => {
       const html = BillsUI({ data: bills })
       document.body.innerHTML = html
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML) 
@@ -86,6 +109,7 @@ describe("Given I am connected as an employee", () => {
 
 import firebase from "../__mocks__/firebase"
 import mail from "../assets/svg/mail.js"
+import { async } from 'rsvp'
 
 // test d'intégration GET
 describe("Given I am a user connected as employee", () => {
